f278ec6d08cb056243afc6293b05d196c1fdd293
ManiGovindan <mani.govindan@outlook.com>
2021-09-15 13:26:20 +0100

    Added transfer between accounts

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -10,4 +10,9 @@ public class Account {
     public void withdraw(int i) {
         balance -= i;
     }
+
+    public void transfer(int i, Account receiver) {
+        this.withdraw(i);
+        receiver.deposit(i);
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -9,29 +9,31 @@ import static org.junit.Assert.assertThat;
 public class AccountTest {
 
     @Test
-    public void startingBalanceIsZero() {
+    public void newAccountShouldHaveZeroBalance() {
         Account account = new Account();
         assertThat(account.balance, is(0));
     }
 
     @Test
-    public void depositAnAmountToIncreaseTheBalance() {
+    public void depositToEmptyAccountShouldIncreaseTheBalance() {
         Account account = new Account();
-        account.deposit(100);
+        assertThat(account.balance, is(0));
 
+        account.deposit(100);
         assertThat(account.balance, is(100));
     }
 
     @Test
-    public void depositingMultipleAmounts() {
+    public void depositToNonEmptyAccountShouldIncreaseTheBalance() {
         Account account = new Account();
         account.deposit(100);
+        assertThat(account.balance, is(100));
         account.deposit(300);
         assertThat(account.balance, is(400));
     }
 
     @Test
-    public void withdrawAmounts() {
+    public void withdrawalShouldDecreaseTheBalance() {
         Account account = new Account();
         account.deposit(300);
 
@@ -40,5 +42,20 @@ public class AccountTest {
 
         account.withdraw(100);
         assertThat(account.balance, is(200));
+    }
+
+    @Test
+    public void transferShouldMoveMoneyFromOneAccountToAnother() {
+        Account sender = new Account();
+        Account receiver = new Account();
+
+        sender.deposit(100);
+        assertThat(sender.balance, is(100));
+        assertThat(receiver.balance, is(0));
+
+        sender.transfer(50,receiver);
+        assertThat(sender.balance, is(50));
+        assertThat(receiver.balance, is(50));
+
     }
 }
